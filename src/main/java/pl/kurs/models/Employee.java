99f86ca1d93a494d6ca47c837eae6ff9cdc2b8f5
Michal Sloniewski <michal.sloniewski@smartcodecreations.com>
2025-11-29 11:44:21 +0100

    Added constructor

--- src/main/java/pl/kurs/models/Employee.java
+++ src/main/java/pl/kurs/models/Employee.java
@@ -5,4 +5,10 @@ public class Employee {
     private String firstName;
     private String lastName;
     private double salary;
+
+    public Employee(String firstName, String lastName, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
 }
